4c0e7acd59f5d0ab00c624e723cf5a3ca74e8570
Melissa Oliveira <107162134+MelissaOliveiraC@users.noreply.github.com>
2023-05-03 00:36:03 -0300

    Update bfs.js

--- bfs.js
+++ bfs.js
@@ -1,6 +1,4 @@
-/*
-    chave = nó's (cidade do grafo), associa então a lista de nó's adjacentes (das outras cidades próximas, parentes)
-*/
+//chave = nó's (cidade do grafo), associa então a lista de nó's adjacentes (das outras cidades próximas, parentes)
 let possibilidades = {
     'Arad': ['Zerind', 'Sibiu', 'Timisoara'],
     'Pitesti': ['Rimnicu Vilcea', 'Craiova', 'Bucharest'],
@@ -40,10 +38,11 @@ function buscarCaminho(origem, destino, possibilidades) {
       }
       //lista de vizinhos do nó-atual
       possibilidades[no]
-        //para cada vizinho não visitado, includes testa se está no array, negando a lógica "!" -> filtra-se apenas vizinhos não estão em visitados.  
+        //para cada vizinho não visitado, includes testa se está no array, negando a lógica "!" -> filtra-se apenas vizinhos que não estão em visitados.  
         .filter(vizinho => !visitados.includes(vizinho))
 
-        //adiciona na lista de visitados, na fila define nó-atual como parente desse vizinho.
+        //adiciona na lista de visitados
+        //na fila define nó-atual como parente desse vizinho.
         .forEach(vizinho => {
           visitados.push(vizinho);
           fila.push(vizinho);
@@ -57,5 +56,4 @@ let origem = 'Arad' // --> a cidade de origem (ponto de partida)
 let destino = 'Bucharest' // --> a cidade final (objetivo)
 
     let caminho = buscarCaminho(origem, destino, possibilidades);
-
     console.log(`O caminho mais rápido entre ${origem} e ${destino} é > ${caminho.join(' --> ')}.`);
